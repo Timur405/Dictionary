5e117273f9f10ee822a3ddf65b79dcbcbee3d0ac
TimurProg <timurnurgayanov@gmail.com>
2026-01-04 11:36:45 +0500

    Улучшили код решение задачи

--- map/Задача 2/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ map/Задача 2/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -9,42 +9,25 @@ int main(){
 	cin >> n;
 
 
-	map<string, int>school = {
-		{"1", 0},
-		{"2", 0},
-		{"3", 0}
+	map<int, vector<int>>school = {
+		{1, vector<int>{}},
+		{2, vector<int>{}},
+		{3, vector<int>{}}
 	};
 
-
-	vector<int>ind1, ind2, ind3;
-
 	for (int i = 0; i < n; i++) {
-		string people; 
+		int people; 
 		cin >> people;
-
-		if (people == "1") {
-			school["1"] += 1;
-			ind1.push_back(i + 1);
-		}
-
-		else if (people == "2") {
-			school["2"] += 1;
-			ind2.push_back(i + 1);
-		}
-
-		else {
-			school["3"] += 1;
-			ind3.push_back(i + 1);
-		}
+		school[people].push_back(i+1);
 	}
 
-	int imin = school["1"];
+	int imin = school[1].size();
 
-	if (imin > school["2"]) {
-		imin = school["2"];
+	if (imin > school[2].size()) {
+		imin = school[2].size();
 	}
-	if (imin > school["3"]) {
-		imin = school["3"];
+	if (imin > school[3].size()) {
+		imin = school[3].size();
 	}
 
 	if (imin == 0) {
@@ -54,9 +37,9 @@ int main(){
 		cout << imin << endl;
 
 		for (int q = 0; q < imin; q++) {
-			cout << ind1[q] << " "
-				<< ind2[q] << " "
-				<< ind3[q] << endl;
+			cout << school[1][q] << " "
+				<< school[2][q] << " "
+				<< school[3][q] << endl;
 		}
 	}
 }
