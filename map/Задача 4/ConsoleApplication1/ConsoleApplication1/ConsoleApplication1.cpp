6c01631d593b05e1b19be8e285482b8e2c507665
TimurProg <timurnurgayanov@gmail.com>
2026-01-05 00:17:12 +0500

    Выполнил задание 4. Ссылка на задачу есть. На следующий день хочу ее доработать

---
+++ map/Задача 4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿//https://codeforces.com/problemset/problem/1669/B
+#include <map>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	int t; 
+	cin >> t;
+	vector<int>hueta;
+	for (int i = 0; i < t; i++) {
+		int n;
+		cin >> n;
+		map<int, int>lmao;
+		vector<int>hui;
+
+		for (int q = 0; q < n; q++) {
+			int a;
+			cin >> a;
+			lmao[a]++;
+			hui.push_back(a);
+		}
+
+
+		bool Flag =0;
+		for (int l = 0; l < hui.size(); l++) {
+			if (lmao[hui[l]] >= 3) {
+				hueta.push_back(hui[l]);
+				Flag = 1;
+				break;
+			}
+		}
+
+
+		if (Flag == 0) {
+			hueta.push_back(-1);
+		}
+	}
+
+
+	for (int pl = 0; pl < hueta.size(); pl++) {
+		cout << hueta[pl] << endl;
+	}
+}
