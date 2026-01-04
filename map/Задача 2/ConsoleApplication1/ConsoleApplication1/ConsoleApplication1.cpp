8cffdfcf949fc06d47e0141277a3f9166fdbc87f
TimurProg <timurnurgayanov@gmail.com>
2026-01-04 11:12:14 +0500

    Использовали словарь для ускорения кода

--- map/Задача 2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ map/Задача 2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -17,18 +17,10 @@ int main() {
 			var.push_back("OK");
 		}
 		else {
-			int a = 1;
-			while (true) {
-				if (log.count(name + to_string(a)) == 0) {
-					log[name + to_string(a)] = 0;
-					var.push_back(name + to_string(a));
-					break;
-				}
-				else{
-					a++;
-				}
-			}
+			log[name]++;
+			var.push_back( name + to_string(log[name]));
 		}
+		
 	}
 	for (int i = 0; i < var.size(); i++) {
 		cout << var[i] << endl;
